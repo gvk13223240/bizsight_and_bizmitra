0460965b4ed26afcf417736944b91379c5505a7d
gvk13223240 <gvk13223240@gmail.com>
2026-01-03 18:32:30 +0530

    Add new host to ALLOWED_HOSTS in settings

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bizsight-hq7l.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bizsight-hq7l.onrender.com", "bizsightandbizmitra-production.up.railway.app"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
