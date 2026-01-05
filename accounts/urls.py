d33d3bdafc45864f9690919ad1c143fef5b4c385
gvk13223240 <gvk13223240@gmail.com>
2026-01-05 17:42:07 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,12 +6,12 @@ from .views import (
     business_dashboard,
     onboarding_view,
     signup_view,
-    activate_view,
+    # activate_view,
 )
 
 urlpatterns = [
     path("signup/", signup_view, name="signup"),
-    path("activate/<uidb64>/<token>/", activate_view, name="activate"),
+    # path("activate/<uidb64>/<token>/", activate_view, name="activate"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("business/profile/", business_profile_view, name="business_profile"),
